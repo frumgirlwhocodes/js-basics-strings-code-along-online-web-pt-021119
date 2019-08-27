a8206efa5e5a0136f8b82d43cb4beb5a1708e24a
lieby weintraub <liebahinda@gmail.com>
2019-08-27 19:44:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
-const currentUser = 'Lieby Weintraub';
-const welcomeMessage = 'Welcome to Flatbook, ';
+const currentUser = 'Grace Hopper';
+ 
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
